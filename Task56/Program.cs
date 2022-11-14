3ea662a4f3f3db3331292c1b3e95910bdf27fbdc
Ayra <777aira777@gmail.com>
2022-11-14 20:35:14 +0400

    Решена 56 задача

---
+++ Task56/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SmallestSum(int[,] matr)
+{
+    int minSum = 0;
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum = sum + matr[i, j];
+        }
+
+        if (i == 0)
+        {
+            minSum = sum;
+            count = 1;
+        }
+
+        if (sum < minSum)
+        {
+            minSum = sum;
+            count = i + 1;
+        }
+        sum = 0;
+
+    }
+
+    return count;
+
+
+}
+
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2D);
+int smallestSum = SmallestSum(array2D);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов => {smallestSum}");
+
+
+
+
